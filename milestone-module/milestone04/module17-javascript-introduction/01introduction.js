c71563fe26ea10e7bdf4960e8fba00a9b8046592
crescentpartha <crescentpartha406@gmail.com>
2022-01-27 22:09:16 +0600

    Module17: 01introduction.js & 02variable.js files added

---
+++ milestone-module/milestone04/module17-javascript-introduction/01introduction.js
@@ -0,0 +1,21 @@
+
+// Introduction to JavaScript, Run Javascript in VSCode
+
+// What is JavaScript?
+// History of JavaScript?
+
+// JavaScript Run:
+//     Browser(inspect)
+//     VSCode
+//     Codepen
+
+// Inspect → Console → Write Code
+console.log(7); // 7
+// 25+62;
+// 54-32;
+
+// Compiler Vs Interpreter
+
+// Source code (hello.c) → Compiler → Machine code → Run the program → Result.
+// Source code (hello.js) → Interpreter → Result
+
---
+++ milestone-module/milestone04/module17-javascript-introduction/02variable.js
@@ -0,0 +1,32 @@
+
+// What is variable, five things you need to declare a variable
+
+// Variable Declaration:
+//     var variable_name = variable_value;
+
+var price = 50;
+var age = 21; 
+var temperature = 37;
+
+console.log(price); // 50
+console.log(age); // 21
+console.log(temperature); // 37
+
+// Numeric type variable 
+var classNumber = 7;
+console.log(classNumber); // 7
+
+// String type variable
+var name = "Crescent Partha";
+var university = 'SUST';
+
+console.log(name); // Crescent Partha
+console.log(university); // SUST
+
+// Boolean type variable 
+var pass = true;
+var subscribed = false;
+
+console.log(pass); // true
+console.log(subscribed); // false
+
