eef385ee78f2627aaf3fde45e11e825819bfb1fa
crescentpartha <crescentpartha406@gmail.com>
2022-01-27 11:49:10 +0600

    Module18: 01variable.js added

---
+++ milestone-module/milestone04/module18-array-and-conditionals/01variable.js
@@ -0,0 +1,15 @@
+// Number 
+var watchPrice = 120;
+console.log(watchPrice);
+watchPrice = 150;
+console.log(watchPrice);
+
+// String 
+var userName = 'Crescent Partha';
+console.log(userName);
+
+// Boolean 
+var isExpensive = true;
+console.log(isExpensive);
+// Integer: 1 2 3 4 5 145
+// Float: 2.714 5.98745 3.1416
