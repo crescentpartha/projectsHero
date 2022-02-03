141c64bae61c13146ea0e377e494f0989b60fa44
crescentpartha <crescentpartha406@gmail.com>
2022-02-04 02:44:19 +0600

    Module18: 10practice-problem file added

---
+++ milestone-module/milestone04/module18-array-and-conditionals/10practice-problem.js
@@ -0,0 +1,22 @@
+
+// Practice Problem - 01
+
+const grade = 50;
+
+if (grade < 50) console.log('Grade F');
+else if (grade >= 50 && grade < 60) console.log('Grade D');
+else if (grade >= 60 && grade < 70) console.log('Grade C');
+else if (grade >= 70 && grade < 80) console.log('Grade B');
+else if (grade >= 80 && grade < 90) console.log('Grade A');
+else if (grade >= 90) console.log('Grade A+');
+
+
+// Practice Problem - 02
+
+const signal = 'yellow';
+
+if (signal == 'red') console.log('Stop');
+else if (signal == 'yellow') console.log('Ready to stop');
+else if (signal == 'green') console.log('Run');
+else console.log('Nothing to do');
+
