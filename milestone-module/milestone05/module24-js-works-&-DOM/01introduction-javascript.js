9a5af20517c711c72d556d8b4d5346a6c58c69e3
crescentpartha <crescentpartha406@gmail.com>
2022-02-07 04:10:18 +0600

    Module24: JavaScript Intro | Compiler vs Interpreter and How works

---
+++ milestone-module/milestone05/module24-js-works-&-DOM/01introduction-javascript.js
@@ -0,0 +1,68 @@
+
+// 24.1 Module Introduction and what is JavaScript
+
+// Q1: What is JavaScript?
+/* 
+JavaScript is a high-level, often just-in-time compiled language that conforms to the ECMAScript standard. 
+It has dynamic typing, prototype-based object-orientation, and first-class functions. 
+It is multi-paradigm, supporting event-driven, functional, and imperative programming styles. 
+It has application programming interfaces (APIs) for working with text, dates, regular expressions, standard data structures, and the Document Object Model (DOM).
+*/
+// Javascript is a client-site application to create web appliction and it is single threated, high-level  interpreted programming language.
+
+// Q2: What is Programming Language?
+// Programming languages are one kind of computer language, and are used in computer programming to implement algorithms.
+
+// Q3: What is Computer Language?
+// A computer language is a formal language used in communication with a computer.
+
+/* 
+// Q4: What is the difference between a machine language vs high-level language?
+Machine Language / Assembly Languages / Low-level Languages:
+    01. A low-level languages are machine-oriented, hardware specific and not portable.
+    02. Machine code is to be read/executed by the computer
+
+High-level Languages:
+    01. High level code is Portable code can run on multiple computer systems without modification.
+    02. A high-level language is user-oriented
+*/
+
+// Both machine code and assembly languages are hardware specific and not portable. This means that the machine code used to run a program on one specific computer needs to be modified to run on another computer. Portable code in a high-level language can run on multiple computer systems without modification.
+// High-level code is designed to be read by human programmers. Machine code is to be read/executed by the computer High level code can be portable/translated for different machines Machine code is specific to a particular machine.
+// A high-level language is one that is user-oriented. A low-level language is machine-oriented.
+
+/* 
+// Q5: Compiled Language vs Interpreted Language?
+Compiled Language:
+    01. A compiled language is a programming language whose implementations are typically compilers and not interpreters.
+    02. Once the program is compiled it is expressed in the instructions of the target machine.
+    03. There are at least two steps to get from source code to execution.
+    04. Compiled programs run faster than interpreted programs.
+    05. Compilation errors prevent the code from compiling.
+    06. The code of compiled language can be executed directly by the computer’s CPU.
+    07. This language delivers better performance.
+    08. Example of compiled language – C, C++, C#, CLEO, COBOL, etc.
+
+Interpreted Language:
+    01. An interpreted language is a programming language whose implementations execute instructions directly and freely, without previously compiling a program into machine-language instructions.
+    02. The instructions are not directly executed by the target machine.
+    03. There is only one step to get from source code to execution.
+    04. Interpreted programs can be modified while the program is running.
+    05. All the debugging occurs at run-time.
+    06. A program written in an interpreted language is not compiled, it is interpreted.
+    07. This languages delivers relatively slower performance.
+    08. Example of Interpreted language – JavaScript, Perl, Python, Java, BASIC, etc.
+*/
+
+/* 
+How Compiler Works: 
+    Source-Code --> Compiler --> Machine-Code --> Output
+How Interpreter Works: 
+    Source-Code --> Interpreter --> Output 
+*/
+
+// *** Must Visited 
+// https://techdifferences.com/difference-between-compiler-and-interpreter.html
+// diagram of compiler and interpreter
+
+
