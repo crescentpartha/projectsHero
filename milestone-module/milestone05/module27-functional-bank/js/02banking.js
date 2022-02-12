b6a07487418a1f84d717367dd821aa3d0258392f
crescentpartha <crescentpartha406@gmail.com>
2022-02-12 13:49:09 +0600

    Module27: Add function

--- milestone-module/milestone05/module27-functional-bank/js/02banking.js
+++ milestone-module/milestone05/module27-functional-bank/js/02banking.js
@@ -1,10 +1,32 @@
 
+/* function doubleIt(num) {
+    const result = num * 2;
+    return result;
+}
+const first = doubleIt(5);
+const second = doubleIt(7); */
+
+function getInputValue(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const AmountValue = parseFloat(inputAmountText);
+
+    inputField.value = ''; // set entered deposit empty
+    return AmountValue;
+}
+
+
+
 // 27.2 Get User deposit and set deposit total value 
 document.getElementById('deposit-button').addEventListener('click', function(){
     // get entered deposit
+
+    /* // Don't Repeat Yourself --> DRY
     const depositInput = document.getElementById('deposit-input');
     const depositAmountText = depositInput.value;
     const depositAmount = parseFloat(depositAmountText);
+    depositInput.value = ''; // set entered deposit empty */
+    const depositAmount = getInputValue('deposit-input'); 
     // console.log(depositAmount);
 
     // get current deposit and set current deposit
@@ -13,7 +35,6 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const previousDepositTotal = parseFloat(depositTotalText);
     depositTotal.innerText = previousDepositTotal + depositAmount; // update current deposit
     // console.log(depositTotalText);
-    depositInput.value = ''; // set entered deposit empty
 
     // 27.3 Update deposit and balance and handle string add
 
@@ -28,10 +49,14 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 // 27.4 Money withdraw and reduce balance for withdraw | handle withdraw button
 document.getElementById('withdraw-button').addEventListener('click', function(){
     // get entered withdraw
+    
     // console.log('withdraw clicked');
+    /* // Don't Repeat Yourself --> DRY
     const withdrawInput = document.getElementById('withdraw-input');
     const withdrawAmountText = withdrawInput.value;
     const withdrawAmount = parseFloat(withdrawAmountText);
+    withdrawInput.value = ''; // clear withdraw input field */
+    const withdrawAmount = getInputValue('withdraw-input');
 
     // console.log(withdrawAmountText);
 
@@ -41,7 +66,6 @@ document.getElementById('withdraw-button').addEventListener('click', function(){
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
 
     withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
-    withdrawInput.value = ''; // clear withdraw input field
 
     // update balance after withdraw
     const balanceTotal = document.getElementById('balance-total');
