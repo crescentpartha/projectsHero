212ea755211a64ccde0f1fb5dc09fa379ee62584
crescentpartha <crescentpartha406@gmail.com>
2022-02-12 13:19:39 +0600

    Module27: Money withdraw and reduce balance for withdraw

--- milestone-module/milestone05/module27-functional-bank/js/02banking.js
+++ milestone-module/milestone05/module27-functional-bank/js/02banking.js
@@ -3,14 +3,52 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
     // get entered deposit
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    // console.log(depositAmount);
+
     // get current deposit and set current deposit
     const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
-    depositTotal.innerText = depositAmount; // update current deposit
-    console.log(depositTotalText);
+    const previousDepositTotal = parseFloat(depositTotalText);
+    depositTotal.innerText = previousDepositTotal + depositAmount; // update current deposit
+    // console.log(depositTotalText);
     depositInput.value = ''; // set entered deposit empty
+
+    // 27.3 Update deposit and balance and handle string add
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount; // update current balance
+
+});
+
+// 27.4 Money withdraw and reduce balance for withdraw | handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    // get entered withdraw
+    // console.log('withdraw clicked');
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+
+    // console.log(withdrawAmountText);
+
+    // get current withdraw and set current withdraw
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+    withdrawInput.value = ''; // clear withdraw input field
+
+    // update balance after withdraw
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
 
 });
 
