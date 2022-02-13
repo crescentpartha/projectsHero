3e9b3f4fc75b08ea52f0462cda1bce4b2943a85f
crescentpartha <crescentpartha406@gmail.com>
2022-02-13 12:12:18 +0600

    Module28: shopping cart handle decrease and update price

--- milestone-module/milestone05/module28-shopping-cart/app.js
+++ milestone-module/milestone05/module28-shopping-cart/app.js
@@ -1,9 +1,27 @@
 
-// 28.1 Module Overview and shopping cart increase case count
-document.getElementById('case-plus').addEventListener('click', function(){
-    // console.log('clicked');
+// 28.2 shopping cart handle decrease and update price
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+    let caseNumber = caseInput.value;
+    if (isIncreasing) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
     console.log(caseInput.value);
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+
+}
+
+// 28.1 Module Overview and shopping cart increase case count
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber(true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
 });
