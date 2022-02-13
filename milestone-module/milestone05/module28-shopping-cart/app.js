9a9f38575a7966bf52475fc60cd7c159750a2e84
crescentpartha <crescentpartha406@gmail.com>
2022-02-13 12:42:13 +0600

    Module28: calculate phone cost using the same shared function

--- milestone-module/milestone05/module28-shopping-cart/app.js
+++ milestone-module/milestone05/module28-shopping-cart/app.js
@@ -1,27 +1,37 @@
 
 // 28.2 shopping cart handle decrease and update price
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
     if (isIncreasing) {
-        caseNumber = parseInt(caseNumber) + 1;
+        productNumber = parseInt(productNumber) + 1;
     }
-    else if (caseNumber > 0) {
-        caseNumber = parseInt(caseNumber) - 1;
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
-    console.log(caseInput.value);
-    // update case total
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
-
+    productInput.value = productNumber;
+    // update product total
+    const productTotal = document.getElementById(product +'-total');
+    productTotal.innerText = productNumber * price;
+    // console.log('Product =', product, productInput.value, productTotal.innerText);
 }
 
+// 28.3 Calculate phone cost using the same shared function
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, true);
+});
+
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, false);
+});
+
 // 28.1 Module Overview and shopping cart increase case count
+// handle case increase decrease events
 document.getElementById('case-plus').addEventListener('click', function () {
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function () {
-    updateCaseNumber(false);
+    updateProductNumber('case', 59, false);
 });
