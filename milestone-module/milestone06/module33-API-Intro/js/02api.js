0509cfc7f6b575a93244f8969273bff075569545
crescentpartha <crescentpartha406@gmail.com>
2022-02-22 00:45:30 +0600

    Module33: JSON placeholder, GET / fetch data, display data on UI

---
+++ milestone-module/milestone06/module33-API-Intro/js/02api.js
@@ -0,0 +1,39 @@
+
+// 33.3 JSON placeholder, GET data, display data on UI
+
+// JSON placeholder
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1') // GET data
+// .then(response => response.json()) // convert to json()
+// .then(json => console.log(json)) // display data on UI
+
+/* display data:
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    }
+Or
+    {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
+*/
+
+/* // 3rd line replaced by 
+.then(json => console.log(json.userId))
+.then(json => console.log(json.title))
+.then(json => console.log(json.id))
+.then(json => console.log("Hello World!")) */
+
+// function loadData() {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+// }
+
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(data => console.log(data.title))
+}
+
+
