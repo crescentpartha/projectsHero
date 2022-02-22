f096bc43633da7d8eb273885b7cebc694f20f2bf
crescentpartha <crescentpartha406@gmail.com>
2022-02-23 01:52:23 +0600

    Module34: International Travel, Display countries name, capital

---
+++ milestone-module/milestone06/module34-API-examples/js/03countries.js
@@ -0,0 +1,29 @@
+
+// 34.3 International Travel, Display countries name, capital
+
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+loadCountries();
+
+const displayCountries = counties => {
+    console.log(counties);
+    // for (const country of counties) {
+    //     console.log(country);
+    // }
+    const countriesDiv = document.getElementById('countries');
+    counties.forEach(country => {
+        console.log(country);
+        const div = document.createElement('div');
+        div.classList.add('country');
+        const h3 = document.createElement('h3');
+        h3.innerText = country.name.common;
+        div.appendChild(h3);
+        const p = document.createElement('p');
+        p.innerText = country.capital;
+        div.appendChild(p);
+        countriesDiv.appendChild(div);
+    });
+}
