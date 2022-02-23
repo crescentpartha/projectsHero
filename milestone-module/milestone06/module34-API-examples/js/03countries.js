c800be6f5ed9f7ba7b45c859af70fda73842284b
crescentpartha <crescentpartha406@gmail.com>
2022-02-23 11:21:54 +0600

    Module34: Call dynamic API, load dynamic data to display

--- milestone-module/milestone06/module34-API-examples/js/03countries.js
+++ milestone-module/milestone06/module34-API-examples/js/03countries.js
@@ -9,21 +9,42 @@ const loadCountries = () => {
 loadCountries();
 
 const displayCountries = counties => {
-    console.log(counties);
+    // console.log(counties);
     // for (const country of counties) {
     //     console.log(country);
     // }
     const countriesDiv = document.getElementById('countries');
     counties.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name.common;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        // 34.4 (advanced) Call dynamic API, load dynamic data to display
+        div.innerHTML = `
+            <h3>${country.name.common}</h3>
+            <p>${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+        `;
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name.common;
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // div.appendChild(p);
         countriesDiv.appendChild(div);
     });
+}
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]));
+}
+const displayCountryDetail = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+        <h4>${country.name.common}</h4>
+        <p>Population: ${country.population}</p>
+        <img width='200px' src="${country.flags.png}">
+    `
 }
