7382ddfbd423f7f21a37ab2cb38984360666fe23
crescentpartha <crescentpartha406@gmail.com>
2022-02-23 13:32:57 +0600

    Module34: Create dynamic url based on click and display data

--- milestone-module/milestone06/module34-API-examples/js/04meal-db.js
+++ milestone-module/milestone06/module34-API-examples/js/04meal-db.js
@@ -19,11 +19,11 @@ const searchFood = () => {
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            <div class="card">
+            <div onclick="loadMealDetail(${meal.idMeal})" class="card" h-100>
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
@@ -33,4 +33,30 @@ const displaySearchResult = meals => {
         `;
         searchResult.appendChild(div);
     })
+}
+
+// 34.7 Create dynamic url based on click and display data
+const loadMealDetail = mealId => {
+    // console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = meal => {
+    console.log(meal);
+    const mealDetail = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.style.marginBottom = '10px';
+    div.innerHTML = `
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+            <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+        </div>
+    `;
+    mealDetail.appendChild(div);
 }
