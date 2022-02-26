0f903713e1bbb780d1aceb27650e9c78547a4c85
crescentpartha <crescentpartha406@gmail.com>
2022-02-26 22:30:41 +0600

    Module34.5: Set autoload | Use arrow function and forEach loop

--- milestone-module/milestone06/module34.5-recap-and-more-API/js/01json.js
+++ milestone-module/milestone06/module34.5-recap-and-more-API/js/01json.js
@@ -1,26 +1,34 @@
 
 // 01. Go to jsonplaceholder website and fetch data to see in console. Ex: comments API
 
-// 02. which data of comments are you see in console, now you try to see in html. Set button to load data and display in website.
+// 02. which data of comments are you load/see in console, now you try to see in html. Set button to load data and display in website.
 
-function loadComments (){
+// 03. Set autoload in website without clicking any button And try to show atleast two property of this data.
+
+// 04. Use arrow function and forEach loop in your code.
+
+const loadComments = () => {
     fetch('https://jsonplaceholder.typicode.com/comments')
     .then(res => res.json())
     .then(data => displayComments(data))
 }
 
-function displayComments(comments){
+// loadComments();
+
+const displayComments = comments => {
     console.log(comments);
     const postComments = document.getElementById('display-comments');
-    for (const comment of comments) {
+    // for (const comment of comments) {
+    comments.forEach(comment => {
         // console.log(comment);
         const div = document.createElement('div');
         div.classList.add('comments-all');
         div.innerHTML = `
-        <h2>${comment.name}</h2>
-        <p>${comment.email}</p>
-        <p>${comment.body}</p>
+        <h2>Name: ${comment.name}</h2>
+        <p>Email: ${comment.email}</p>
+        <p>Information: ${comment.body}</p>
         `;
         postComments.appendChild(div);
-    }
+
+    })
 }
