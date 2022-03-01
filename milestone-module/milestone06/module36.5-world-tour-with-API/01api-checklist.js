8d9fcf8f4a1adddda95e8c73d6de08a7643169fc
crescentpartha <crescentpartha406@gmail.com>
2022-03-02 03:11:30 +0600

    Module36.5: concept milestone practice

---
+++ milestone-module/milestone06/module36.5-world-tour-with-API/01api-checklist.js
@@ -0,0 +1,23 @@
+
+// 36.5.1 API ES6 and concept milestone practice
+
+/* 
+    1. Object with property (string, boolean, number, object, array, method(function) which will return a property value)
+    2. template string 
+        -- minimum 3 property will be dynamic
+        -- nested object property
+        -- array second element
+        -- another property
+    3.1 No parameter arrow function which return 89
+    3.2 Single parameter arrow function. will divide input by 7
+    3.3 arrow function with two parameters. return  (a+b)/2
+    3.4 multiline arrow function. two parameter. return (a+7)+(b+7)
+    4. takes array and apply map to divide each element by 7 
+    5. map, forEach, filter, find
+    6. Destructuring on object and array (second element = second)
+    7. fetch photos API and show photos and title in html using card by bootstrap
+    8. After clicking any photo, it shows thumbnail, image, title in upper div. 
+    9. Use arrow function and forEach loop through your loaded data.
+    10. Watch again "the meal db" (33.5 - 33.8) and practice "the coktaildb". Implement search functionality and show details after clicking any item.
+    
+*/
