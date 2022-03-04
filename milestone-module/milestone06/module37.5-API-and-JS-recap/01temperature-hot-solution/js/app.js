362ad0eb9d43b5e0173e4737873bf94f613fb224
crescentpartha <crescentpartha406@gmail.com>
2022-03-04 16:00:40 +0600

    Module27.5: Display temperature, city, dynamic weather icon

--- milestone-module/milestone06/module37.5-API-and-JS-recap/01temperature-hot-solution/js/app.js
+++ milestone-module/milestone06/module37.5-API-and-JS-recap/01temperature-hot-solution/js/app.js
@@ -4,13 +4,26 @@
 const API_KEY = `723a18053ef7f48f45406ac8e74c5c42`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperature(data))
 }
 
+// 37.5.2 Display temperature, city, dynamic weather icon
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
 const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
     console.log(temperature);
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
 }
 
