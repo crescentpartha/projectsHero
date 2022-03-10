ba06da74e66be1682d9d80dfb2673387738b7c67
crescentpartha <crescentpartha406@gmail.com>
2022-03-10 23:24:15 +0600

    Module38: Explore Browser & Debug Theoretically

---
+++ milestone-module/milestone07/module38-JS-and-browser-works/00milestone-overview.js
@@ -0,0 +1,41 @@
+
+// 38.0 Milestone Overview
+
+// After this milestone, we are starting to learn REACT
+
+/* # What I know from this Milestone:
+    01. How JS and Browser works
+    02. How Internet works
+    03. DOM + CSSOM = Render Tree 
+    04. DOM Tree + CSS Tree Structure = Render Tree
+    05. How V8 works | V8 Internal Mechanism
+        JS Code --> Parse it --> Abstract Syntax Tree --> Generate bytecode 
+        --> Optimize & Compile it (by TURBOFAN)(Just in time Compiler) --> Run Optimized Code
+    06. setTimeout() | setInterval() | ClearInterval()
+    07. Event Loop, Concurrency
+    08. Browser API and Methods
+    09. BOM Vs DOM
+    10. Browser API
+    11. Alert() | Confirm() | Prompt()
+    12. URL Parts:  Protocol --> Resource Name (Sub-Domain, Domain Name, Top-level Domain) --> File Path
+    13. Uses of History API
+    14. Cookies
+    15. Local Storage | Session Storage | Cookie
+    16. JS Debug, Web Debug, Dev Tool Mastering
+    17. More Debug & Regular Expression
+    
+    # What I need to know to jump REACT?
+    01. 6 JS fundamentals
+    02. Template String
+    03. Arrow function
+    04. Spread operation
+    05. Array methods 
+    06. Array & Object Destructuring
+    07. Optional Chaining
+    08. JSON, fetch, keys, values
+    09. Array add or remove using dots
+    10. Truthy or falsy
+    11. Ternary operation
+*/
+
+
---
+++ milestone-module/milestone07/module38-JS-and-browser-works/01how-internet-works.js
@@ -0,0 +1,15 @@
+
+// 38.1 Module Introduction and how internet works
+
+/* 
+    01. What is my IP Address? (IPv4 Vs IPv6)
+    02. What is DNS (Domain Name System)? How DNS works?
+    03. TCP IP Layers
+    04. What is the TCP/IP Model & How does it work?
+    05. What is Protocol?
+    06. HTTPS - Hypertext Transfer Protocol Secuure 
+    07. How Does Internet Works? Link: (https://www.youtube.com/watch?v=x3c1ih2NJEg)
+
+    # Sarver - Data - Name - DNS - IP Address - Internet
+*/
+
