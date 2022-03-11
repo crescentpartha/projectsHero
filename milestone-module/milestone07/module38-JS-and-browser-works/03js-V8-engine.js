d196e317af9cc396cf4c168dba56ec3b143dcf00
crescentpartha <crescentpartha406@gmail.com>
2022-03-11 11:14:19 +0600

    Module38: JavaScript V8 Engine Internal Mechanism

---
+++ milestone-module/milestone07/module38-JS-and-browser-works/03js-V8-engine.js
@@ -0,0 +1,22 @@
+
+// 38.3 JavaScript Engine V8 Internal Mechanism
+
+/* 
+    01. JavaScript V8 Engine (Link: https://v8.dev/)
+    02. How JavaScript V8 Engine Works? (Image: https://miro.medium.com/proxy/1*ZIH_wjqDfZn6NRKsDi9mvA.png)
+        JavaScript = Interpreter Language
+        C++ = Compiler Language
+        For Fast JS Code Execution: Just in time compiler (Used)
+    03. V8 Just in time Compiler (JIT Compiler)
+    04. How JavaScript works: Optimizing the V8 compiler for efficiency
+        (Article: https://blog.logrocket.com/how-javascript-works-optimizing-the-v8-compiler-for-efficiency/)
+    05. Steps of V8 Engine
+        (Link1: https://static.tildacdn.com/tild3533-6537-4639-b961-393433393837/1_GuWInZljjvtDpdeT6O.png)
+        (Link2: https://assets.digitalocean.com/articles/alligator/js/v8-engine/javascript_journey.png)
+        (Link3: https://media.geeksforgeeks.org/wp-content/uploads/20211002235143/workinggfg.png)
+
+    V8 Engine ==> JIT Comiler + TurboFan (Optimization) + Garbage Collection
+    V8 Engine Works in Browser & Sarver
+
+*/
+
