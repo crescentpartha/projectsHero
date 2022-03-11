dd9d29178e7a0ccd8afffbabbd3e2b1f913767ee
crescentpartha <crescentpartha406@gmail.com>
2022-03-11 14:20:11 +0600

    Module38: JavaScript Event Loop and Concurrency

---
+++ milestone-module/milestone07/module38-JS-and-browser-works/07eventLoop-concurrency.js
@@ -0,0 +1,15 @@
+
+// 38.7 (advanced) JavaScript event loop and concurrency
+
+/* 
+    01. JavaScript Event Loop - MDN web docs
+        (link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop)
+    02. JavaScript Concurrency Model and Event Loop
+        (Link: https://www.freecodecamp.org/news/javascript-concurrency-model-and-event-loop/)
+    03. What the heck is the event loop anyway? | Philip Roberts
+        (Link: https://www.youtube.com/watch?v=8aGhZQkoFbQ)
+    04. JSConf
+        (Link: https://www.youtube.com/channel/UCzoVCacndDCfGDf41P-z0iA)
+*/
+
+
