fbdbaff8ccab4f04b0184118123b161aedeea8f1
crescentpartha <crescentpartha406@gmail.com>
2022-03-12 15:02:04 +0600

    Module39: Module Summary, ContentEditable, chrome restart/newtab/version

---
+++ milestone-module/milestone07/module39-browser-api-&-methods/08summary.js
@@ -0,0 +1,45 @@
+
+// 39.9 Module Summary, ContentEditable, chrome restart
+
+/* 
+    Browser have one/some API. We can use "document & fetch" using those API. "document & fetch" doesn't work without browser.
+
+    alert('');
+    confirm('Are you single?');
+    prompt('name');
+
+    Location & URL Parts
+    http://127.0.0.1:5500/milestone-module/milestone07/module39-browser-api-&-methods/07shopping/07shopping.html
+    location.reload()
+    location.href = ''
+    location.hash = '#partha'
+
+    history.back()
+    history.forward()
+    history.go()
+
+    cookies
+    sessionStorage
+    localStorage
+
+    sessionStorage Vs localStorage
+    cookies Vs sessionStorage Vs localStorage
+
+    Web APIs = https://developer.mozilla.org/en-US/docs/Web/API
+    Clipboard API
+    Console API
+    DOM
+    Storage
+    Fetch API
+
+    IN console panel: 
+        1. document.body.contentEditable // "inherit"
+        2. document.body.contentEditable = true // Then full website is editable
+        3. chrome://newtab (in address bar) // open new tab
+        4. chrome://version  // display details
+        5. chrome://restart  // Closed & Restart chrome browser
+
+        Shift + Esc = display which applications are consume memory
+        Ctrl + L = select URL
+*/
+
