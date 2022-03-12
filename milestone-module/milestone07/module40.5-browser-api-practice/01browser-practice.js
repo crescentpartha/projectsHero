dc580fc035a978337df230e220b28f7bec601810
crescentpartha <crescentpartha406@gmail.com>
2022-03-12 23:46:13 +0600

    Module40.5: Added Practice Questions

---
+++ milestone-module/milestone07/module40.5-browser-api-practice/01browser-practice.js
@@ -0,0 +1,34 @@
+
+// 40.5.1 Practice Browser API with checklist and a must work
+
+/* 
+1. console.log in setTimeout and Show the output after 3.5 seconds later.
+2. take a number using prompt, add 200. then alert the result.
+3. do you want to see the href (using confirm). if ok then console.log href (in alert)
+4. purpose of cookies. 5-7 lines in english
+5. 3 differences between local storage and session storage.
+
+------------------------
+        HOMEWORK
+------------------------
+
+6. how javaScript run? 5-7 lines in english
+7. list of things that are asynchronous in JS/browser
+8. What is event loop in JS and how does it work?
+
+------------------------
+        OPTIONAL
+------------------------
+
+9. simple website with two input fields and a button. product name (1st input field), product price (2nd input field) 
+    by clicking on the button product will be added to the local storage. You should be able to add multiple.
+10. Display products on the websites. If they exists on the local storage. Display from there.
+
+------------------------
+        MUST
+------------------------
+
+11. stack overflow, create an account. spend 30 minute.
+
+*/
+
