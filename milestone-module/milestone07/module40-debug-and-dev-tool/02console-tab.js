13defd28cd2b2997f947bd60e82609fd984a2d68
crescentpartha <crescentpartha406@gmail.com>
2022-03-12 20:34:40 +0600

    Module40: Console tab, preserve log, log levels, console api

---
+++ milestone-module/milestone07/module40-debug-and-dev-tool/02console-tab.js
@@ -0,0 +1,34 @@
+
+// 40.3 Console tab, preserve log, log levels, console api
+
+/* 
+    document.getElementById('phone-plus');
+    querySelector()
+    $('#phone-plus') //shortcut like jQuery
+
+    console.log
+    Common Logging Levels: https://dzone.com/articles/logging-levels-what-they-are-and-how-they-help-you
+    console.info('normal info')
+    console.warn('Do not come in front of my special one')
+    console.error('user password is wrong')
+    clear() | Ctrl + L
+    monitor(updateProductNumber) | monitor(function_Name) --> then click // Show full details
+    unmonitor(updateProductNumber) | unmonitor(function_Name) --> then click
+
+    const phonePlus = $('#phone-plus')
+    phonePlus
+    monitorEvents(phonePlus) // Show all events
+    unmonitorEvents(phonePlus)
+    monitorEvents(phonePlus, ['click', 'mouseout']) // Only show click, mouseout event.
+    unmonitorEvents(phonePlus)
+
+    console.count('update fn clicked');
+
+    Chrome dev tool console API: https://developer.chrome.com/docs/devtools/console/api/
+    Chrome dev tool console Utilities API: https://developer.chrome.com/docs/devtools/console/utilities/
+
+    const phonePlus = document.getElementById('phone-plus')
+    getEventListeners(phonePlus) // display list of events in an array
+
+*/
+
