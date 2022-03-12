dd6b9efae42d18ac4ddf0212ccbcffdb901e0967
crescentpartha <crescentpartha406@gmail.com>
2022-03-12 21:11:39 +0600

    Module40: Sources tab, break point, call stack, event break

---
+++ milestone-module/milestone07/module40-debug-and-dev-tool/03sources-tab.js
@@ -0,0 +1,12 @@
+
+// 40.4 Sources tab, break point, call stack, event break
+
+/* 
+    Ctrl + G = type line number // go to certain line
+    Ctrl + Shift + O = to find function // search function name
+    Esc = To open console in the botton when use sources tab or other tab
+*/
+
+/* 
+    Breakpoints, Call Stack, XHR/fetch Breakpoints, DOM Breakpoints, Event Listener Breakpoints
+*/
