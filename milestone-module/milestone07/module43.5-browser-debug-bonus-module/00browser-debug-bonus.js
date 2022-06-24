eccf15e774a01fbf76ee060d7982f8f25e05d1a6
crescentpartha <crescentpartha406@gmail.com>
2022-06-25 01:54:26 +0600

    Browser & Debug Milestone Bonus Tips & Tricks

---
+++ milestone-module/milestone07/module43.5-browser-debug-bonus-module/00browser-debug-bonus.js
@@ -0,0 +1,39 @@
+
+// 43.5 Browser & Debug Milestone Bonus Tips & Tricks
+
+/* 
+    # Five(5) Technic of debuging: 
+        0. Error/Issue Reproduce: check red signal in the console panel | create similer/same error (manytimes)
+        1. Error point Backward: set breakpoint in the (error/red-signal) | debug/console from backward to foreward.
+        2. Error point Forward: set breakpoint in the (error/red-signal) | debug/console from foreward to backward.
+        3. less use breakpoint & more use console.log:
+        4. comments in the error section & Elimination Technique:
+        5. Combination Technique: One technique isn't enough.
+
+    ** Milestone 07: Browser & Debug **
+
+    # Five Things:
+        1. How does work JavaScript?
+        2. How does work Browser?
+            Link: https://web.dev/howbrowserswork/
+            Link: https://developer.mozilla.org/en-US/docs/Web/Performance/How_browsers_work
+        3. Parts of URL
+        4. Location API - To show different parts of URL
+        5. Local Storage vs Session Storage? When use?
+
+    # Ten Things:
+        1. What is JavaScript? (google)
+        2. How does work (setTimeout & setInterval) | Difference
+        3. How to use History API
+        4. Difference among alert, confirm, and prompt | When use?
+        5. Documentations of devtool (google)
+        6. TypeScript vs JavaScript
+        7. What is event loop?
+        8. What is Accessibility?
+        9. What is call stack?
+        10. Regular Expressions | When & How use?
+
+*/
+
+
+
