b7deb3951af2d64cb6cdc9f8ee054130fe228239
crescentpartha <crescentpartha406@gmail.com>
2022-06-25 15:55:36 +0600

    Web(React) component - Types of component - Identify component

---
+++ milestone-module/milestone08-my-react-app/milestone08/module44-modern-front-end-core-concepts/01component.js
@@ -0,0 +1,21 @@
+
+// 44.2 Web component, 4 types of component, identify component
+
+/* 
+    Documentation of React(reactjs.org) = https://reactjs.org/docs/getting-started.html
+    Exclusive React Workshop for Beginners! = https://www.thatjsdude.com/
+    React Tutorial = https://www.w3schools.com/react/default.asp
+
+    React Components:
+        (Identify Component)-(We can see different parts of website from different angle)
+        1. Similer in Look, different in Data
+        2. Container: Contains many [(Similer in Look, different in Data) x n]
+        3. No Pattern, but breakdown for working purpose
+        4. Stand-Along Component (Stand along the part of website)
+
+        - Components are independent and reusable bits of code.
+        - They serve the same purpose as JavaScript functions, but work in isolation and return HTML.
+        - Two Types: Class components & Function components
+*/
+
+
