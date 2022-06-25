f0b40f7c697069edc539e62dd79eeb341fc8cd99
crescentpartha <crescentpartha406@gmail.com>
2022-06-26 03:51:09 +0600

    Website's State, State Management, Purpose of State

---
+++ milestone-module/milestone08/module44-modern-front-end-core-concepts/05state.js
@@ -0,0 +1,54 @@
+
+// 44.7 Website state, state management, purpose of state
+
+/* 
+    Links:
+        1. https://www.simplilearn.com/tutorials/asp-dot-net-tutorial/state-management-in-asp-net
+        2. https://www.techtarget.com/searchapparchitecture/definition/state-management
+        3. https://www.codemag.com/article/0409061/State-Management
+
+    State:
+        - Water (liquid, solid, gas - State change)
+        - Gmail (unread message to read message (unread-message -= 1) - State change)
+        - E-commomerce Site (Add to cart (cart++) - State change)
+        - Website (LogIn (create session) - State change)
+        - Facebook (Link react, love react, comment (num++) - State change)
+
+        ----------------------------------------------------------------------------------
+
+        - State can be changed or unchanged depends on Users.
+        - State change means situation changed
+        - If any user interact with website, then state change.
+        - Some website does not have user interact state. (Static website)
+        - State is something could change.
+        - If something in the website could change it's called State.
+
+    State Management:
+        - It refers to managing the state of one or several user interface control systems like that of radio controls, action buttons, input fields, etc.
+        - It is a programming technique for User Interface in which the state of a single UI control completely or partially depends on the state of all the other UI controls.
+
+        - Application state management is the process of maintaining knowledge of an application's inputs across multiple related data flows 
+          that form a complete business transaction -- or a session -- to understand the condition of the app at any given moment.
+        - In computer science, an input is information put into the program by the user and state refers to the condition of an application 
+          according to its stored inputs -- saved as variables or constants.
+        - State can also be described as the collection of preserved information that forms a complete session.
+
+
+    Types of State Management:
+        1. Client-side / Front-end
+            - Cookies
+            - Viewstate
+            - Control state
+            - Query String
+            - Hidden Field
+        2. Server-side / Back-end
+            - Session
+            - Application
+            - Cache
+
+    Purpose of State:
+        - When you have state management in place data actually flows from your app to state and vice versa. You know exactly where your data is. These state management tools also give you a point-in-time snapshot of the entire data. In that way, you know exactly where your data is and that makes your development faster.
+
+*/
+
+
