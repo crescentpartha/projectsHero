1d2e2199a1d02381099b9ccdc102382b744f7a2b
crescentpartha <crescentpartha406@gmail.com>
2022-06-25 20:06:47 +0600

    SPA vs MPA

---
+++ milestone-module/milestone08/module44-modern-front-end-core-concepts/03single-page-application.js
@@ -0,0 +1,62 @@
+
+// 44.4 Single Page Application (SPA), Performance Benefit
+
+/* 
+    SPA with Pros and Cons and Examples: https://www.netsolutions.com/insights/single-page-application/
+    SPA vs MPA: https://www.pluralsight.com/guides/why-do-we-need-a-single-page-application
+    SPA vs MPA: https://echoinnovateit.com/single-page-app-vs-multi-page-app/#:~:text=Examples%20of%20Multi%20page%20applications,they%20are%20low%20on%20speed.
+
+    SPA: 
+        1. A SPA application is a single page that continuously interacts with the user by dynamically rewriting the current page 
+            rather than loading entire new pages from a server.
+
+        Pros: 
+            - Quick Loading Time
+            - User Experience (better)
+            - Easy debugging
+            - Easy to develop and maintain (easy to add advanced features to a web application)
+            - Better caching
+            - Better performance (Uses Less Bandwidth)
+        Cons:
+            - SEO Optimization (Doesn’t Perform Well With SEO)
+            - Uses a Lot of Browser Resources
+            - Security Issues
+            - Scalability Problems
+
+        Most-Popular Single Page Application Frameworks:
+            - Meteor | React.JS | Angular.JS | Vue.JS | Backbone.JS | Ember.JS
+
+        Single Page Application Examples:
+            - Gmail | GitHub | Facebook | Twitter | Google Maps | Netflix | Grammarly | Trello | Spotify | Instagram | WhatsApp
+
+    ------------------------------------------------------------------------------------------------------------------------------------
+
+    MPA:
+        1. A multi-page Application is the traditional web application where a new page is requested from the server to display 
+            when data is exchanged back and forth. 
+        2. The amount of content they carry is enormous.
+        3. Thus, they are generally multi-level deep with a considerable number of links and intricate UIs.
+
+        Pros:
+            - Better SEO optimization (you can target your audience easily)
+            - follow a high-security standard
+            - Scalability (Scale as much as you like)
+            - Easy Analysis of website
+        Cons:
+            - Slow performance
+            - Higher resource utilization
+            - complex to develop and maintain (More time for development and maintenance)
+            - Less flexibility
+            - Less reusability
+
+        Multi-Page Application Examples:
+            - Amazon | eBay
+
+        When used:
+            - E-Commerce Websites, Blogs, Forums
+            - Sell products and various services
+
+    Choosing SPA-MPA: depends on business requirements
+*/
+
+
