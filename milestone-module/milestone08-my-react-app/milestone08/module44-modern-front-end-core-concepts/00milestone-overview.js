5ddce08b80aed0a49f4f0e6d0a6dcb5d736166d6
crescentpartha <crescentpartha406@gmail.com>
2022-06-25 14:19:43 +0600

    Milestone Overview | Modern Front-End core concepts

---
+++ milestone-module/milestone08-my-react-app/milestone08/module44-modern-front-end-core-concepts/00milestone-overview.js
@@ -0,0 +1,59 @@
+
+// 44.0 Milestone Overview
+
+/* 
+    ** Milestone-08: I will know from this milestone **
+
+    Modern Front-End Core Concepts:
+        1. Web component
+        2. Dynamically create HTML elements
+        3. Single Page Application (SPA)
+        4. Routing, Route parameter
+        5. Destructuring and props
+        6. Website state
+        7. Folder structure
+
+    REACT Core Concepts:
+        1. Core concepts Discussion
+        2. Working with JSX, Dynamic content, Dynamic Style
+        3. Pass dynamic data to components, props
+        4. Create multiple components
+        5. State, component state hook and set state
+        6. Load dynamic data, API call useEffect integrate state
+
+    How REACT Works:
+        1. What is JSX, React Component, babel transpiler
+        2. Components and how props works
+        3. Unidirectional data flow
+        4. How states works, asynchronous
+        5. How React Hook works
+        6. Props vs state
+        7. Properties vs attributes
+        8. What is React, when to use it
+        9. Render, virtual dom, diff algorithm
+        10. React component lifecycle
+
+    I will also learn:
+        1. ES6 Modules
+        2. Import, Export, Default
+        3. Relative Path
+        4. Create and Load Fake data
+        5. Array reduce
+        6. Local storage functionality
+        7. Good UI vs bad UI
+        8. Themeforest, freepik, flaticon, unsplash
+        9. Add or remove key to an object
+        10. Wrap event handler with arrow function 
+        11. Local storage with more functionalities
+
+    Besides, the project I will explore with react from this milestone:
+        1. Rest countries Project
+        2. E-commerce project Ema-John (Home - Shop - Order Review)
+
+    Some Quotation:
+        1. To get something you never had, you have to do something you never did!
+        2. Checkmate your circumstances & go ahead!!
+        3. 😄See you in Victory!!!
+*/
+
+
