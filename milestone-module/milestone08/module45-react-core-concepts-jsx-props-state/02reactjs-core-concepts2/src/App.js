50b0b67b17a6148744edfd9fe6054e6c8d71be60
crescentpartha <crescentpartha406@gmail.com>
2022-06-27 00:35:39 +0600

    Concept Recap - JSX - components - props - display array of objects

--- milestone-module/milestone08/module45-react-core-concepts-jsx-props-state/02reactjs-core-concepts2/src/App.js
+++ milestone-module/milestone08/module45-react-core-concepts-jsx-props-state/02reactjs-core-concepts2/src/App.js
@@ -2,12 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'laptop', price: 153000},
+    {name: 'phone', price: 80000},
+    {name: 'watch', price: 5300},
+    {name: 'tablet', price: 52000},
+  ]
+
   return (
     <div className="App">
-      
+      {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
 
+      {/* <Product name="laptop" price="50000"></Product>
+      <Product name="phone" price="75000"></Product>
+      <Product name="watch" price="3500"></Product> */}
     </div>
   );
+}
+
+function Product(props) {
+  return (
+    <div className="product">
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
+    </div>
+  )
 }
 
 export default App;
