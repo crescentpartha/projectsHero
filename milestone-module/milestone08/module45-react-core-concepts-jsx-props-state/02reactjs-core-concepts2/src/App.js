6a7ac60b8ffb3dc1cfc30c0f7d7a49062163bdea
crescentpartha <crescentpartha406@gmail.com>
2022-06-27 01:10:34 +0600

    State, component state hook and set state method

--- milestone-module/milestone08/module45-react-core-concepts-jsx-props-state/02reactjs-core-concepts2/src/App.js
+++ milestone-module/milestone08/module45-react-core-concepts-jsx-props-state/02reactjs-core-concepts2/src/App.js
@@ -1,34 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    {name: 'laptop', price: 153000},
-    {name: 'phone', price: 80000},
-    {name: 'watch', price: 5300},
-    {name: 'tablet', price: 52000},
-  ]
-
+  
   return (
     <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-
-      {/* <Product name="laptop" price="50000"></Product>
-      <Product name="phone" price="75000"></Product>
-      <Product name="watch" price="3500"></Product> */}
+      <Counter></Counter>
     </div>
   );
 }
 
-function Product(props) {
+function Counter() {
+  // useState() functional component এ state variable কে রাখতে দেয়।
+  // const [] = useState(0);
+  
+  const [count, setCount] = useState(55); // Returns a stateful value, and a function to update it.
+  
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  
+  // const increaseCount = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+
+  // console.log(abc);
+
+  return (
+    <div>
+      <h1>Count (State Change): {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+/* 
+  Object distructuring:
+  {name, age} = {name: 'a', age: 7}
+
+  Array distructuring:
+  [first, second] = [44, 11]
+*/
+
+// (45.6) Concept Recap, JSX, components, props, display array of objects
+
+/* const products = [
+  {name: 'laptop', price: 153000},
+  {name: 'phone', price: 80000},
+  {name: 'watch', price: 5300},
+  {name: 'tablet', price: 52000},
+] */
+
+/* {
+  products.map(product => <Product name={product.name} price={product.price}></Product>)
+}
+<Product name="laptop" price="50000"></Product>
+<Product name="phone" price="75000"></Product>
+<Product name="watch" price="3500"></Product> */
+
+/* function Product(props) {
   return (
     <div className="product">
       <h3>Name: {props.name}</h3>
       <p>Price: {props.price}</p>
     </div>
   )
-}
+} */
 
 export default App;
