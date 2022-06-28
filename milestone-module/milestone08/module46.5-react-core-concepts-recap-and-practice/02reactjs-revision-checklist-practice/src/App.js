85bd2c7f67eb94effbbfd4f42689c58365b47b15
crescentpartha <crescentpartha406@gmail.com>
2022-06-29 01:26:35 +0600

    create blog(article) & blogs components | show the properties by props

--- milestone-module/milestone08/module46.5-react-core-concepts-recap-and-practice/02reactjs-revision-checklist-practice/src/App.js
+++ milestone-module/milestone08/module46.5-react-core-concepts-recap-and-practice/02reactjs-revision-checklist-practice/src/App.js
@@ -1,12 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+const articleStyle = {
+  color: 'darkBlue',
+  fontSize: 'xx-large',
+  textAlign: 'center',
+  textTransform: 'capitalize', 
+  textShadow: '2px 2px 2px gray'
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <article className="blog">
+        <h2 style={articleStyle}>Create React App</h2>
+        <p style={{textAlign: 'justify', fontWeight: '500'}}>Create React App is a comfortable environment for learning React, and is the best way to start building a new single-page application in React.
+          It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, 
+          and optimizes your app for production.</p>
+      </article>
+      <Blog 
+      heading="Components and Props" 
+      author="Andrew Clark" 
+      content="Conceptually, components are like JavaScript functions. They accept arbitrary inputs (called “props”) and return React elements describing what should appear on the screen."
+      ></Blog>
+      <Blog 
+      heading="Handling Events" 
+      author="Rick Hanlon" 
+      content="Handling events with React elements is very similar to handling events on DOM elements. There are some syntax differences: React events are named using camelCase, rather than lowercase. And With JSX you pass a function as the event handler, rather than a string."
+      ></Blog>
+      <Blog 
+      heading="Thinking in React" 
+      author="Luna Ruan" 
+      content="One of the many great parts of React is how it makes you think about apps as you build them. In this document, we’ll walk you through the thought process of building a searchable product data table using React."
+      ></Blog>
     </div>
   );
+}
+
+function Blog(props) {
+  return (
+    <div className="blogs">
+      <h2>{props.heading}</h2>
+      <h5>Author: {props.author}</h5>
+      <p>{props.content}</p>
+    </div>
+  )
 }
 
 export default App;
