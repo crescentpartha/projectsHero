95b60dc592f9c37e2b00cebac22244e68ec08e8e
crescentpartha <crescentpartha406@gmail.com>
2022-07-06 03:08:26 +0600

    set eventhandler, pass eventhander to child

--- milestone-module/milestone08/module48-simple-react-SPA-with-simple-E-commerce/01ema-john-simple/src/components/Product/Product.js
+++ milestone-module/milestone08/module48-simple-react-SPA-with-simple-E-commerce/01ema-john-simple/src/components/Product/Product.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Product.css';
 
+/* 
+    → In React, We can pass data from up to down
+    → eventhandler need to puts in shop component sothat we can pass data as a props in a cart.
+    → We can pass data (object, function) as a props
+*/
+
 const Product = (props) => {
     // console.log(props.product);
     const {name, img, seller, price, ratings } = props.product;
+    // const {handleAddToCart} = props;
     return (
         <div className='product'>
             <img src={img} alt="img" />
@@ -13,7 +20,7 @@ const Product = (props) => {
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Ratings: {ratings} stars</small></p>
             </div>
-            <button className='btn-cart'>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
                 <p>Add to Cart</p>
             </button>
         </div>
