5f163f11053751c5c4cbc2747dceb21de4b41b6a
crescentpartha <crescentpartha406@gmail.com>
2022-07-06 02:15:56 +0600

    Product Component, component layout, display data

--- milestone-module/milestone08/module48-simple-react-SPA-with-simple-E-commerce/01ema-john-simple/src/components/Shop/Shop.js
+++ milestone-module/milestone08/module48-simple-react-SPA-with-simple-E-commerce/01ema-john-simple/src/components/Shop/Shop.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
     useEffect( () => {
+        // fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data));
@@ -13,7 +15,12 @@ const Shop = () => {
     return (
         <div className='shop-container'> 
             <div className="products-container">
-                <h3>This is for products: {products.length}</h3>
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product={product}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
