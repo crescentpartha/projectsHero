ad6bcbd7e1a02b79f021ef4ad338e3c163edf273
crescentpartha <crescentpartha406@gmail.com>
2022-07-09 19:18:18 +0600

    style changed slightly

--- milestone-module/milestone08/module49.5-Simple-React-Revision-Day/01food-uganda-practice/src/components/Summary/Summary.js
+++ milestone-module/milestone08/module49.5-Simple-React-Revision-Day/01food-uganda-practice/src/components/Summary/Summary.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Summary.css';
 
 const Summary = (props) => {
-    console.log(props.cart);
+    // console.log(props.cart);
     return (
         <div className='summary'>
             <h2>Order Summary</h2>
             <p>Total cart Added: {props.cart.length}</p>
             {
                 props.cart.map(product => displayCartProduct(product))
+                // props.cart.map(product => console.log(product))
             }
             <div id='cart-product'>
 
@@ -17,31 +18,24 @@ const Summary = (props) => {
     );
 };
 
-function setNameStyle(name) {
-    name.style.color = 'orange';
-    name.style.backgroundColor = 'bisque';
-    name.style.padding = '5px';
-    name.style.border = '2px solid red';
-    name.style.borderRadius = '10px';
-    name.style.margin = '5px';
-}
-
 function setContainerStyle(container) {
     container.style.backgroundColor = '#ffeacc';
     container.style.padding = '5px 0';
+    container.style.margin = '5px';
 }
 
 const displayCartProduct = (product) => {
     // console.log(product.strMeal);
     const {strMeal} = product;
-    // console.log(strMeal);
+    console.log(strMeal);
     const container = document.getElementById("cart-product");
     container.innerHTML = "";
     const name = document.createElement('div');
+    name.classList.add('product-name');
     name.innerHTML = `
     <p>${strMeal}</p>
     `;
-    setNameStyle(name);
+    // setNameStyle(name);
     setContainerStyle(container);
     container.appendChild(name);
 }
