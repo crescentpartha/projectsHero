ad8ed6e482b7e47721543e3233897f853206890a
crescentpartha <crescentpartha406@gmail.com>
2022-07-15 01:52:30 +0600

    Get from local storage and handle quantity property

--- milestone-module/milestone08/module50.5-simple-react-bonus/03react-meal-db-initial/src/components/OrderList/OrderList.js
+++ milestone-module/milestone08/module50.5-simple-react-bonus/03react-meal-db-initial/src/components/OrderList/OrderList.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const OrderList = (props) => {
     const { orders } = props;
+    console.log(orders);
 
-    
+    // we can solve it in one line code by using Reduce. Try it.
+    let count = 0; 
+    for (const meal of orders) {
+        count = count + meal.quantity;
+    }
+
     return (
         <div>
             <h2>Order List</h2>
-            <h4>Items Ordered: {orders.length}</h4>
+            <h4>Items Ordered: {count}</h4>
         </div>
     );
 };
