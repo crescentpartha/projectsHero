784cdcc74d8aadb08770d7dc1bcbc11f97b8a448
crescentpartha <crescentpartha406@gmail.com>
2022-08-05 12:56:28 +0600

    Implement Search with simple code

--- milestone-module/milestone09/module55.5-react-router-bonus-content/01simple-volunteers/src/components/Main/Main.js
+++ milestone-module/milestone09/module55.5-react-router-bonus-content/01simple-volunteers/src/components/Main/Main.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 import Activity from '../Activity/Activity';
 import useVolunteers from '../../hooks/useVolunteers';
 
 const Main = () => {
     const [volunteers] = useVolunteers();
+    // const [searchResult, setSearchResult] = useState(volunteers); // initially set all volunteers data to set volunteers.
+    const [searchResult, setSearchResult] = useState([]);
+
+    const handleSearchChange = event => {
+        // console.log(event.target.value);
+        const searchText = event.target.value;
+        const match = volunteers.filter(v => v.title.toLowerCase().includes(searchText));
+        setSearchResult(match);
+    }
 
     return (
         <div>
-            <h2>Main</h2>
             <h2>Volunteer Activities: {volunteers.length}</h2>
+            <div style={{'margin': '20px'}}>
+                <input onChange={handleSearchChange} type="text" placeholder='search' />
+            </div>
             <div className="activity-container">
                 {
-                    volunteers.map(activity => <Activity
+                    searchResult.map(activity => <Activity
                         key={activity._id}
                         activity={activity}
                     ></Activity>)
