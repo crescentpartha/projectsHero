6d6e27457d9863776fc0bae48f4d400d29b62280
crescentpartha <crescentpartha406@gmail.com>
2022-08-05 12:27:03 +0600

    Use Custom Hook to share hook code among components

--- milestone-module/milestone09/module55.5-react-router-bonus-content/01simple-volunteers/src/components/Header/Header.js
+++ milestone-module/milestone09/module55.5-react-router-bonus-content/01simple-volunteers/src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
+        <nav>
             <h2>Header</h2>
             <Link to='/home'>Home</Link>
-            <Link to='/volunteers'>Volunteers</Link>
             <Link to='/main'>Main</Link>
+            <Link to='/volunteers'>Volunteers</Link>
             <Link to='/notFound'>Not-Found</Link>
-        </div>
+        </nav>
     );
 };
 
