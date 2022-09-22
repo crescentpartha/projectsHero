d88c93355c105681acfd776ff52793dd29930e24
crescentpartha <crescentpartha406@gmail.com>
2022-09-23 03:07:32 +0600

    Create Routes for Project Setup

--- milestone-module/milestone11/module66.5-CRUD-Update-and-Product-Management/01crud-product-management/src/App.js
+++ milestone-module/milestone11/module66.5-CRUD-Update-and-Product-Management/01crud-product-management/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import AddProducts from './components/AddProducts/AddProducts';
+import UpdateProducts from './components/UpdateProducts/UpdateProducts';
+import ManageProducts from './components/ManageProducts/ManageProducts';
+import NotFound from './components/NotFound/NotFound';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to CRUD-Project-Management</h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/addProducts' element={<AddProducts></AddProducts>}></Route>
+        <Route path='/updateProducts' element={<UpdateProducts></UpdateProducts>}></Route>
+        <Route path='/manageProducts' element={<ManageProducts></ManageProducts>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- milestone-module/milestone11/module66.5-CRUD-Update-and-Product-Management/04crud-product-management.md
+++ milestone-module/milestone11/module66.5-CRUD-Update-and-Product-Management/04crud-product-management.md
@@ -4,6 +4,7 @@ Table of Contents
 - [CRUD Product Management](#crud-product-management)
   - [`Basic Project Setup`](#basic-project-setup)
     - [`Project Setup 01` (Basic Setup)](#project-setup-01-basic-setup)
+    - [`Project Setup 02` (Create Routes)](#project-setup-02-create-routes)
 
 # CRUD Product Management
 
@@ -15,5 +16,16 @@ Table of Contents
 - Create ___instruction___ documentation
 - Create ___project___ documentation
 
+### `Project Setup 02` (Create Routes)
+
+- Install and Setup React Router
+  - Wrap `<App />` component by using `<BrowserRouter>` in ___src/index.js___ file
+  - Create some ___Components___ such as Home, Header, AddProducts, ManageProducts, UpdateProducts, and NotFound.
+  - Maintain ___File-Structure___ or ___Folder-Structure___
+  - Then, ___Add some Routes___ in ___src/App.js___ file
+- Set `_redirects` file in ___public___ folder
+- Install ___React-Bootstrap___ and Add ___Bootstrap CDN Link___ inside the ___index.html___
+- Change ___project title___
+- Add ___favicon.ico___
 
 
